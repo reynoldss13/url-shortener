91a60c4d81fdcffdb6b2f0408378bd41a3ba3043
sreynolds85 <stephenreynolds1985@gmail.com>
2022-08-10 18:33:53 +0100

    10/08/22 - CORS

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, make_response, request, jsonify
 from DB.db_access import insert_url, get_url
+import webbrowser
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Insert original URL & return shortened URL
-@app.route("/generate", methods=["GET"])
+@app.route("/generate", methods=["POST"])
 def generate_url():
     if "url" in request.form:
         resp = insert_url(request.form["url"])
@@ -18,13 +21,14 @@ def generate_url():
 
 # Using the given ID and base URL, return the original URL
 @app.route("/<string:id>", methods=["GET"])
-def get_original_url(id):
+def navigate_to_original_url(id):
     try:
         url = get_url(id)
     except:
         return make_response("Error: Invalid URL", 200)
     if url is not None:
-        return make_response(jsonify({"data":url}), 200)
+        webbrowser.open(url)
+        return
     else:
         return make_response("Error: ID not found", 404)
 
